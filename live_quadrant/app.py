19b75420b8b188519b98fe5a0817114c8329dc1f
Mehdi <azadani.mehdi@gmail.com>
2025-02-09 02:35:55 -0800

    Added Seinfeld quadrant

--- live_quadrant/app.py
+++ live_quadrant/app.py
@@ -23,14 +23,44 @@ def get_weather():
     }
 
 
-
 def get_family_photo():
     image_folder = 'static/family_photos/'
     images = [f for f in os.listdir(image_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
     return random.choice(images) if images else 'default_photo.jpg'  # Fallback to default
 
 
+def get_seinfeld_quote():
+    # Fetch quotes from the Seinfeld API
+    local_quotes = [
+        {
+            "quote": "Yada yada yada",
+            "character": "Elaine"
+        },
+        {
+            "quote": "It's not a lie if you believe it.",
+            "character": "George Costanza"
+        },
+        {
+            "quote": "Yeah, and you're an anti-dentite.",
+            "character": "Cosmo Kramer"
+        },
+        {
+            "quote": "The sea was angry that day my friends.",
+            "character": "George Costanza"
+        },
+        {
+            "quote": "That's a shame.",
+            "character": "Jerry Seinfeld"
+        },
+        {
+            "quote": "Serenity now!",
+            "character": "Frank Costanza"
+        }
+    ]
+    selected = random.choice(local_quotes)
+    return selected
 
+    
 @app.route('/')
 def dashboard():
     context = {
@@ -38,13 +68,20 @@ def dashboard():
         'date': datetime.now().strftime('%Y-%m-%d'),
         'temperature': get_temperature(),
         'weather': get_weather(),
-        'family_photo': get_family_photo()
+        'family_photo': get_family_photo(),
+        'seinfeld': get_seinfeld_quote(),
     }
+    print(context)
     return render_template('dashboard.html', **context)
 
 @app.route('/update_photo')
 def update_photo():
     return {'photo': get_family_photo()}
+
+
+@app.route('/update_seinfeld_quote')
+def update_seinfeld_quote():
+    return {"seinfeld": get_seinfeld_quote()}
 
 
 if __name__ == '__main__':
